f4988b426edba278b33300a92af456fe1bd8ce02
lucas brown <lucas20brown@hotmail.co.uk>
2023-01-06 17:59:45 +0000

    added paragraph in for about me, also adjusted line height and p font size

--- src/components/About.js
+++ src/components/About.js
@@ -7,11 +7,14 @@ function About() {
       <div id="about-info">
         <h2 id="job">Full Stack <br></br> Web Developer </h2>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-          It has survived not only five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.
+          I recently graduated from Edge Hill University with a 2:1 in Business & Management.
+          Throughout my final year of university and after I graduated,
+          I realised that I had little interest in the employment options that my degree had prepared me for.
+          After plenty of research, I made the decision to take a chance on a 10-week course at LeWagon London in order to pursue a career in web development.
+          It was a fantastic decision that confirmed my desire to pursue this as a career.
+          I now have a background with Ruby and Ruby on Rails, although some of my most recent projects have been developed using React.
+          As a developer, my goal is to create and design experiences that simplify people's lives.
+          I am also, a huge football fan, especially of Everton Football Club, where you may (sometimes) see me supporting them.
         </p>
       </div>
       <div id="skills-info">
