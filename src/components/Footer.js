d30b3a8a4bfdc69df6eba6099e1c50628cc4e633
lucas brown <lucas20brown@hotmail.co.uk>
2023-01-08 23:13:18 +0000

    added font awsome to footer and carousel

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,22 @@
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub, faReact} from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
   return (
   <div className="footer">
     <div className="footer-links">
-      {/* <FontAwesomeIcon icon="fa-github"/>
-      <FontAwesomeIcon icon="fab fa-instagram"/>
-      <FontAwesomeIcon icon="fab fa-facebook"/>
-      <FontAwesomeIcon icon="fab fa-twitter"/>
-      <FontAwesomeIcon icon="fab fa-linkedin"/> */}
+      <a href="https://www.linkedin.com/in/lucas18brown/" target="_blank" rel="noopener noreferrer" >
+        <FontAwesomeIcon icon={faLinkedin} className="social-icon"/>
+      </a>
+      <a href='https://github.com/Lucas18Brown' target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faGithub} className="social-icon"/>
+      </a>
     </div>
     <div className="footer-copyright">
-      Lucas Brown
+      Made with
+      <a href='https://reactjs.org/' target="_blank" rel='noopener noreferrer'>
+        <span className='font-bold text-sky-400'>React</span> <FontAwesomeIcon icon={faReact} className="text-sky-400 fa-beat"/>
+      </a>
     </div>
   </div>
 )}
