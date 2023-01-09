820752ab37abb4ff15d4842e3bfa799b03963b57
lucas brown <lucas20brown@hotmail.co.uk>
2023-01-09 00:05:53 +0000

    move 1,2 and 3 to render inside project-showcase = less clutter

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import Navbar from './components/Navbar.js';
 import Welcome from './components/Welcome.js';
 import About from './components/About.js';
 import Projects from './components/Projects/Projects.js';
-import Project1 from './components/Projects/Project1.js';
-import Project2 from './components/Projects/Project2.js';
-import Project3 from './components/Projects/Project3.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -18,9 +15,6 @@ root.render(
     <Welcome/>
     <About/>
     <Projects/>
-    <Project1/>
-    <Project2/>
-    <Project3/>
     <Footer/>
   </div>
 );
