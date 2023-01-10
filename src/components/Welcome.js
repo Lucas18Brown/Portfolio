26eee030345e8e24009ed7e7533c21ed11955fce
lucas brown <lucas20brown@hotmail.co.uk>
2023-01-10 12:48:47 +0000

    changed some component layout, to clean it up

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,3 @@
-// import { TypeAnimation } from 'react-type-animation';
-
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     console.log(entry)
-//     entry.target.classList.toggle('show', entry.isIntersecting);
-//   })
-// })
-
-// const hiddenElements = document.querySelectorAll('hidden');
-// hiddenElements.forEach((el) => observer.observe(el));
 
 function Welcome(props) {
   return (
@@ -21,37 +10,6 @@ function Welcome(props) {
         <div className="info-container">
           <h2 id="job">Full Stack <br></br> Web Developer </h2>
           <p id='info'>Looking to create and design experiences that simplify peoples' lives.</p>
-          {/* <TypeAnimation
-            sequence={[
-              1000,
-              'Rails',
-              1000,
-              'React',
-              1000,
-              'Html',
-              1000,
-              'Heroku',
-              1000,
-              'CSS',
-              1000,
-              'SCSS',
-              1000,
-              'JavaScript',
-              1000,
-              'Postgresql',
-              1000,
-              'Ruby',
-
-              () => {
-                console.log('Done typing!'); // Place optional callbacks anywhere in the array
-              }
-            ]}
-            speed={1}
-            deletionSpeed={1}
-            wrapper="TypeAnimation"
-            cursor={true}
-            style={{ fontSize: '32px', color: '#FFC947', fontWeight: '500'}}
-          /> */}
         </div>
       </div>
   )
