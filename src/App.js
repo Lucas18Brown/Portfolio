4c5c8b1064c0db12edad1bb6aeca2ca7855b045c
lucas brown <lucas20brown@hotmail.co.uk>
2023-01-28 17:35:29 +0000

    added title to my page

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import About from './components/About.jsx';
 import Projects from './components/Projects/Projects.jsx';
 import Footer from './components/Footer.jsx';
 import Contacts from './components/Contacts.jsx';
+import { Helmet } from "react-helmet";
 
 const App = () => {
   return (
-    <div className='main-container'>
+  <div className='main-container'>
+    <Helmet>
+      <title>Lucas Brown | Web Developer</title>
+    </Helmet>
     <Navbar/>
     <Welcome/>
     <About/>
